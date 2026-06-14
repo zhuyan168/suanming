8e888d965ac4736aebb0829e7397ec1eee1d1316
zhuyan168 <529885408@qq.com>
2026-06-14 09:17:58 +0800

    shujujiankong

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -124,6 +124,18 @@ function MyApp({ Component, pageProps }: AppProps) {
         <meta name="twitter:image" content={DEFAULT_OG_IMAGE} key="twitter:image" />
       </Head>
 
+      <Script
+        src="https://plausible.io/js/pa-LgxI65kqy0AEp-XDiJnWA.js"
+        strategy="afterInteractive"
+      />
+      <Script id="plausible-analytics" strategy="afterInteractive">
+        {`
+          window.plausible = window.plausible || function(){(plausible.q = plausible.q || []).push(arguments)};
+          plausible.init = plausible.init || function(i){plausible.o = i || {}};
+          plausible.init();
+        `}
+      </Script>
+
       {GA_ID && (
         <>
           <Script
